fdedec3208fb058a93ca873f8a5a39124119050f
HoangPhan <HoangPhan@ocn19.encs.concordia.ca>
2018-03-29 12:15:42 -0400

    added new code of memoryerror of new_clustering

--- new_clustering.py
+++ new_clustering.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 import pyspark.sql.functions as F
 from pyspark.ml.evaluation import RegressionEvaluator
@@ -6,6 +7,8 @@ from pyspark.sql import Row
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 import random
+import copy
+import math
 
 random_seed = 191
 random.seed(random_seed)
@@ -21,6 +24,20 @@ def map_user_product_dict(user_product):
 
 	return tmp_result
 
+def update_centroid_average():
+	global centroid_vectors
+	global K
+
+	for index in range(0, K):
+		cluster_size = len(clusters[index])
+		users_in_cluster = matrix_user_product_rdd.filter(lambda user: user['User_ID'] in clusters[index]).collect()
+		for key in centroid_vectors[index]:
+			current_sum = 0
+			if key != 'User_ID':
+				for user in users_in_cluster:
+					current_sum += user[key]
+			centroid_vectors[index][key] = current_sum / cluster_size
+
 spark = SparkSession.builder.appName("demo-cluster").getOrCreate()
 training = spark.read.csv("dataset/train.csv", header = "true")
 
@@ -34,12 +51,75 @@ user_distinct = training.select('User_ID').distinct().rdd.map(lambda user: user[
 user_product = training.select('User_ID', 'Product_ID').distinct().groupBy('User_ID')\
 	.agg(F.collect_list('Product_ID').alias('Product_ID'))
 
-matrix_user_product = user_product.rdd.map(map_user_product_dict)
+matrix_user_product_rdd = user_product.rdd.map(map_user_product_dict)
+matrix_user_product = matrix_user_product_rdd.collect()
 
-# # Clustering based on number of cities
-# # K = 3
-# K = 3
+# Clustering based on number of cities K = 3
+K = 3
 clusters = [[]] * K
 random_initial_user = random.sample(user_distinct, K)
 
-centroid_vectors = matrix_rdd.filter(lambda state: state["name"] in initial_states).
+centroid_vectors = matrix_user_product_rdd.filter(lambda user: user["User_ID"] in random_initial_user).collect()
+
+# for user_vector in matrix_user_product:
+# 	current_min_distance = sys.maxsize
+# 	min_index = -1
+
+# 	for index in range(0, K):
+# 		current_distance = 0
+# 		for key in centroid_vectors[index]:
+# 			if key != 'User_ID':
+# 				current_distance += math.pow(abs(centroid_vectors[index][key] - user_vector[key]), 2)
+
+# 		if current_distance < current_min_distance:
+# 			current_min_distance = current_distance
+# 			min_index = index
+
+# 	if len(clusters[min_index]) == 0:
+# 		clusters[min_index] = []
+
+# 	clusters[min_index].append(user_vector['User_ID'])
+
+last_centroid_vectors = []
+
+iteration = 0
+
+while centroid_vectors != last_centroid_vectors:
+	last_centroid_vectors = copy.deepcopy(centroid_vectors)
+	clusters = [[]] * K
+
+	iteration += 1
+	print("iteration: " + str(iteration))
+
+	for user_vector in matrix_user_product:
+		current_min_distance = sys.maxsize
+		min_index = -1
+
+		for index in range(0, K):
+			current_distance = 0
+			for key in centroid_vectors[index]:
+				if key != 'User_ID':
+					current_distance += math.pow(abs(centroid_vectors[index][key] - user_vector[key]), 2)
+
+			if current_distance < current_min_distance:
+				current_min_distance = current_distance
+				min_index = index
+
+		if len(clusters[min_index]) == 0:
+			clusters[min_index] = []
+
+		clusters[min_index].append(user_vector['User_ID'])
+	update_centroid_average()
+
+file = open("output_file", "w")
+
+for index in range(0, K):
+	file.write("* CLASS " + str(index) + "\n")
+	# print("* CLASS " + str(index))
+
+	for el in clusters[index]:
+		file.write(el + ' ')
+		# print(el, end=' ')
+	# print()
+	file.write("\n")
+file.close()
