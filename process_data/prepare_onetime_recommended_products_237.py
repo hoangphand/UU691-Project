1c83c1ec9aa2ff4a9c8ba78a87b30f9198b03415
hoangphand <hoangphanduy2015@gmail.com>
2018-04-02 08:24:39 -0400

    done recommender content based 237 based on personal purchase history only

--- process_data/prepare_onetime_recommended_products_237.py
+++ process_data/prepare_onetime_recommended_products_237.py
@@ -14,77 +14,79 @@ test = spark.read.csv("dataset/test.csv", header = "true")
 
 # READ group by cate
 # Cate_Product = Row('Product_Category', 'Product_ID')
-# def map_group_by_category_file(line):
-# 	cate = [int(i) for i in line.value.split(';')[0].split(',')]
-# 	product = line.value.split(';')[1].split(',')
-# 	return (tuple(cate), product)
+def map_group_by_category_file(line):
+	cate = [int(i) for i in line.value.split(';')[0].split(',')]
+	product = line.value.split(';')[1].split(',')
+	return (tuple(cate), product)
 
-# group_by_category_file = spark.read.text("tmp_output_file/group_by_category.ouput").rdd
-# group_by_category = group_by_category_file.map(map_group_by_category_file)
-# group_by_category_dict = group_by_category.collectAsMap()
+group_by_category_file = spark.read.text("tmp_output_file/group_by_category.ouput").rdd
+group_by_category = group_by_category_file.map(map_group_by_category_file)
+group_by_category_dict = group_by_category.collectAsMap()
 
-# # READ user matrix
-# def map_user_products_file(line):
-# 	result = {}
-# 	parts = line.value.split(';')
-# 	# result['User_ID'] = parts[0]
-# 	for index in range(1, len(parts) - 1):
-# 		key = [int(i) for i in parts[index].split(':')[0].split(',')]
-# 		value = int(parts[index].split(':')[1])
-# 		result[tuple(key)] = int(value)
+# READ user matrix
+def map_user_products_file(line):
+	result = {}
+	parts = line.value.split(';')
+	# result['User_ID'] = parts[0]
+	for index in range(1, len(parts) - 1):
+		key = [int(i) for i in parts[index].split(':')[0].split(',')]
+		value = int(parts[index].split(':')[1])
+		result[tuple(key)] = int(value)
 
-# 	return (parts[0], result)
+	return (parts[0], result)
 
-# user_products_file = spark.read.text("tmp_output_file/user_product_matrix_237.ouput").rdd
-# user_product_matrix_rdd = user_products_file.map(map_user_products_file)
+user_products_file = spark.read.text("tmp_output_file/user_product_matrix_237.ouput").rdd
+user_product_matrix_rdd = user_products_file.map(map_user_products_file)
 
-# # GEN randomly recommended products based on a seed
-# no_of_cates = 3
-# no_of_products_for_each_cate = 3
-# random_seed = 191
-# random.seed(random_seed)
+# GEN randomly recommended products based on a seed
+no_of_cates = 6
+no_of_products_for_each_cate = 6
+random_seed = 191
+random.seed(random_seed)
 
-# def map_recommended_products(user):
-# 	top_user_cates = sorted(user[1].iteritems(), key=operator.itemgetter(1), reverse=True)
-# 	user_recommended_products = []
+def map_recommended_products(user):
+	top_user_cates = sorted(user[1].iteritems(), key=operator.itemgetter(1), reverse=True)
+	user_recommended_products = []
 
-# 	max_no_of_products = no_of_products_for_each_cate * no_of_cates
-# 	current_total = 0
-# 	count = 0
+	max_no_of_products = no_of_products_for_each_cate * no_of_cates
+	current_total = 0
+	count = 0
 
-# 	while current_total < max_no_of_products:
-# 		max_to_be_chosen = min(len(group_by_category_dict[top_user_cates[count][0]]), no_of_products_for_each_cate)
-# 		to_be_chosen = min(max_to_be_chosen, max_no_of_products - current_total)
-# 		if to_be_chosen == 0:
-# 			break
-# 		current_total += to_be_chosen
-# 		user_recommended_products.extend(random.sample(group_by_category_dict[top_user_cates[count][0]], to_be_chosen))
-# 		count += 1
+	while current_total < max_no_of_products:
+		max_to_be_chosen = min(len(group_by_category_dict[top_user_cates[count][0]]), no_of_products_for_each_cate)
+		to_be_chosen = min(max_to_be_chosen, max_no_of_products - current_total)
+		if to_be_chosen == 0:
+			break
+		current_total += to_be_chosen
+		# user_recommended_products.extend(random.sample(group_by_category_dict[top_user_cates[count][0]], to_be_chosen))
+		for index in range(0, to_be_chosen):
+			user_recommended_products.append(group_by_category_dict[top_user_cates[count][0]][index])
+		count += 1
 
-# 	return (user[0], user_recommended_products)
+	return (user[0], user_recommended_products)
 
-# recommended_products = user_product_matrix_rdd.map(map_recommended_products).collect()
+recommended_products = user_product_matrix_rdd.map(map_recommended_products).collect()
 
-# file = open("tmp_output_file/one_time_recommended_products_237_3x3_191.ouput", 'w')
-# for row in recommended_products:
-# 	file.write(str(row[0]))
-# 	file.write(';')
+file = open("tmp_output_file/one_time_recommended_products_237_6x6_with_sorting.ouput", 'w')
+for row in recommended_products:
+	file.write(str(row[0]))
+	file.write(';')
 
-# 	for index in range(0, len(row[1])):
-# 		file.write(str(row[1][index]))
-# 		if index != len(row[1]) - 1:
-# 			file.write(',')
-# 	file.write('\n')
-# file.close()
+	for index in range(0, len(row[1])):
+		file.write(str(row[1][index]))
+		if index != len(row[1]) - 1:
+			file.write(',')
+	file.write('\n')
+file.close()
 
 
 # READER
-def map_one_time_recommended_products_file(line):
-	user_id = line.value.split(';')[0]
-	product = line.value.split(';')[1].split(',')
-	return (user_id, product)
+# def map_one_time_recommended_products_file(line):
+# 	user_id = line.value.split(';')[0]
+# 	product = line.value.split(';')[1].split(',')
+# 	return (user_id, product)
 
-one_time_recommended_products_file = spark.read.text("tmp_output_file/one_time_recommended_products_237_3x3_191.ouput").rdd
-one_time_recommended_products = one_time_recommended_products_file.map(map_one_time_recommended_products_file)
-one_time_recommended_products_dict = one_time_recommended_products.collectAsMap()
-print(one_time_recommended_products_dict)
+# one_time_recommended_products_file = spark.read.text("tmp_output_file/one_time_recommended_products_237_3x3_191.ouput").rdd
+# one_time_recommended_products = one_time_recommended_products_file.map(map_one_time_recommended_products_file)
+# one_time_recommended_products_dict = one_time_recommended_products.collectAsMap()
+# print(one_time_recommended_products_dict)
--- recommander_content_based_237.py
+++ recommander_content_based_237.py
@@ -8,105 +8,44 @@ from pyspark.sql.functions import desc
 
 spark = SparkSession.builder.appName("demo-cluster").getOrCreate()
 training = spark.read.csv("dataset/train.csv", header = "true")
+test = spark.read.csv("dataset/test.csv", header = "true")
 
-def map_products_by_categories(product):
-	arr = []
-	if product['Product_Category_1'] != None:
-		arr.append((product['Product_ID'], int(product['Product_Category_1'])))
-	
-	if product['Product_Category_2'] != None:
-		arr.append((product['Product_ID'], int(product['Product_Category_2'])))
-
-	if product['Product_Category_3'] != None:
-		arr.append((product['Product_ID'], int(product['Product_Category_3'])))
-
-	return arr
-
-def get_list_length(list):
-	return len(list)
-
-def get_list_list_length(list):
-	sum = 0
-	for el in list:
-		sum += len(el)
-	return sum
-
-def map_construct_user_product_matrix(user):
-	tmp_matrix = dict(group_by_category_dict)
-	tmp_matrix['User_ID'] = user['User_ID']
-
-	for basket in user['Product_ID']:
-		for product in basket:
-			product_cate = tuple(group_by_product_dict[product])
-			tmp_matrix[product_cate] += 1
-
-	return tmp_matrix
-
-global_order_count = 0
-last_user_id = -1
-
-def add_order_id(current_user_id):
-	global last_user_id
-	global global_order_count
-	if last_user_id == -1:
-		last_user_id = current_user_id
-		global_order_count = 1
-		return global_order_count
-
-	if current_user_id != last_user_id:
-		last_user_id = current_user_id
-		global_order_count = global_order_count + 1
-
-	return global_order_count
-
-udf_add_order_id = udf(add_order_id)
-
-udf_get_list_length = udf(get_list_length)
-udf_get_list_list_length = udf(get_list_list_length)
-
-Product = Row('Product_ID', 'Product_Category')
-def map_group_by_product_file(line):
-	product_cate = line.value.split(';')[1].split(',')
-	return (line.value.split(';')[0], product_cate)
-
-group_by_product_file = spark.read.text("tmp_output_file/group_by_product.ouput").rdd
-group_by_product = group_by_product_file.map(map_group_by_product_file).map(lambda product: Product(*product))
-group_by_product = spark.createDataFrame(group_by_product)
-
-Cate_Product = Row('Product_Category', 'Product_ID')
-def map_group_by_category_file(line):
-	cate = line.value.split(';')[0].split(',')
+# GEN one time recommended products
+def map_one_time_recommended_products_file(line):
+	user_id = line.value.split(';')[0]
 	product = line.value.split(';')[1].split(',')
-	return (cate, product)
+	return (user_id, product)
 
-group_by_category_file = spark.read.text("tmp_output_file/group_by_category.ouput").rdd
-group_by_category = group_by_category_file.map(map_group_by_category_file).map(lambda cate: Cate_Product(*cate))
-group_by_category = spark.createDataFrame(group_by_category)
+one_time_recommended_products_file = spark.read.text("tmp_output_file/one_time_recommended_products_237_6x6_with_sorting.ouput").rdd
+one_time_recommended_products = one_time_recommended_products_file.map(map_one_time_recommended_products_file)
+one_time_recommended_products_dict = one_time_recommended_products.collectAsMap()
 
-group_by_product_dict = group_by_product.rdd.map(lambda product: (product['Product_ID'], product['Product_Category']))\
-	.collectAsMap()
-group_by_category_dict = group_by_category.rdd.map(lambda cate: (tuple(cate['Product_Category']), 0)).collectAsMap()
-# print(group_by_category_dict)
+# GEN orders with actual products purchased from test
+Order = Row('User_ID', 'Order_ID', 'Product_ID')
+def map_test_group_tx_add_order_id_to_dataset_file(line):
+	user_id = line.value.split(';')[0]
+	order_id = line.value.split(';')[1]
+	product_id = line.value.split(';')[2].split(',')
+	return (user_id, order_id, product_id)
 
-# dataset_with_order_id = training.withColumn('Order_ID', udf_add_order_id('User_ID'))
+test_group_tx_add_order_id_file = spark.read.text("tmp_output_file/test_group_tx_add_order_id.ouput").rdd
+test_group_tx_add_order_id = test_group_tx_add_order_id_file.map(map_test_group_tx_add_order_id_to_dataset_file)\
+	.map(lambda order: Order(*order))
+test_group_tx_add_order_id = spark.createDataFrame(test_group_tx_add_order_id)
 
-# user_order = dataset_with_order_id.select('User_ID', 'Order_ID', 'Product_ID').distinct()\
-# 	.groupBy('User_ID', 'Order_ID').agg(F.collect_list('Product_ID').alias('Product_ID'))\
-# 	.orderBy('User_ID', 'Order_ID')
-# user_order_with_count = user_order.withColumn('Quantity', udf_get_list_length('Product_ID')).orderBy('User_ID')
+def evaluate_recommendation(user_id, actual_purchase_products):
+	result = list(set(actual_purchase_products).intersection(one_time_recommended_products_dict[user_id]))
+	if len(result) > 0:
+		return 1
+	else:
+		return 0
 
-# user_order_with_count.show()
+udf_evaluate_recommendation = udf(evaluate_recommendation)
 
-# user_products_without_order_id = user_order.select('User_ID', 'Product_ID')\
-# 	.groupBy('User_ID').agg(F.collect_list('Product_ID').alias('Product_ID'))\
-# 	.withColumn('Quantity', udf_get_list_list_length('Product_ID')).orderBy('User_ID', desc('Quantity'))
-# user_products_without_order_id.show()
+# add colum for evaluation
+test_group_tx_add_order_id = test_group_tx_add_order_id.withColumn('Result', udf_evaluate_recommendation('User_ID', 'Product_ID'))\
+	.orderBy('User_ID', 'Order_ID')
 
-# user_product_matrix_rdd = user_products_without_order_id.rdd.map(map_construct_user_product_matrix)
-# user_product_matrix = user_product_matrix_rdd.collect()
-
-# file = open("tmp_output_file/user_product_matrix_237.ouput", 'w')
-# for user in user_product_matrix:
-# 	file.write(str(user) + '\n')
-# 	# print(user_product_matrix)
-# file.close()
+print(test_group_tx_add_order_id.rdd.filter(lambda row: int(row['Result']) == 1).count())
+#result = test_group_tx_add_order_id.groupBy().sum('Result').collect()
+#print(len(result.rdd.filter(lambda basket: basket['Result'] == 1).collect()))
